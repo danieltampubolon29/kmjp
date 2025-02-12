ad1a70a585ffc8c3f76d6938fe90f0eb9a5dd2b6
danieltampubolon29 <tampubolondaniel29@gmail.com>
2025-02-12 22:45:53 +0700

    Revert "menambahkan fitur login dan crud data anggota"
    This reverts commit f711c5c2eef0845d7ea048143732fa95f73390e2.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\AnggotaController;
 use App\Http\Controllers\Marketing\MarketingController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -20,35 +19,21 @@ require __DIR__.'/auth.php';
 //marketing route
 Route::middleware('auth', 'MarketingMiddleware')->group(function(){
     Route::get('/dashboard', [MarketingController::class, 'index'])->name('marketing.dashboard');
+
+
 });
 
 // admin route
 Route::middleware('auth', 'AdminMiddleware')->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+
+
 });
 
 
-Route::prefix('anggota')->name('anggota.')->group(function() {
-    // Route untuk menampilkan semua anggota
-    Route::get('/', [AnggotaController::class, 'index'])->name('index');
-    
-    // Route untuk menampilkan form untuk membuat anggota baru
-    Route::get('/create', [AnggotaController::class, 'create'])->name('create');
-    
-    // Route untuk menyimpan anggota baru
-    Route::post('/', [AnggotaController::class, 'store'])->name('store');
-    
-    // Route untuk menampilkan form edit anggota
-    Route::get('{anggota}/edit', [AnggotaController::class, 'edit'])->name('edit');
-    
-    // Route untuk memperbarui anggota
-    Route::put('{anggota}', [AnggotaController::class, 'update'])->name('update');
-    
-    // Route untuk menghapus anggota
-    Route::delete('{anggota}', [AnggotaController::class, 'destroy'])->name('destroy');
-    
-    // Route untuk menampilkan detail anggota (optional)
-    Route::get('{anggota}', [AnggotaController::class, 'show'])->name('show');
-});
+
+
+
+
 
 
